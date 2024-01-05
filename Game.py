f4740d7f23dba585f358586e7845872450643928
Saumya Narang <101644310+SaumyaN18@users.noreply.github.com>
2024-01-05 21:26:09 +0530

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,213 @@
+print('________________________________________________________________________________________________________________________')
+print('                                          WELCOME!!!                                                     ')
+print('''
+My dearest of friends , the world has been destroyed due to all the climatic changes...
+I,TIME am shifting you to some other world, but be careful .This world is a little scarier....
+You have 5 chances to survive.
+                                                                                                          ''')
+print('                                 GOOD LUCK MY FRIEND!!!╰*°▽°*╯                                    ')
+print('___________________________________________________________________________________________________________________________')
+
+
+print('''
+You are driving towards your home, the temprature is freezing, even your car cannot
+protect you from the harsh cold. You see a diner.''')
+i=1
+while (i):
+    a=int(input('''
+    Do you dare to approach the diner?
+    1) Yes
+    2) No
+    You choice :-'''))
+    if a==1:
+        print ('''                              Good Job <3 (1/5 stages completed) (*^▽^*)                    ''')
+    else:
+        print('                                 You died from cold!!!!  ╯°□°）╯︵ ┻━┻')
+        break
+    i-=1
+
+
+    print('''
+    You enter the diner , a waiter takes you to your table , but he has no face and only a giant mouth full of sharp teeth.
+    Everyone else sitting at the the table has black eyes. You must escape this place , but since you're one of my dearest friends
+    I shall let you on some hints .''')
+
+    print('''
+    Hints:-
+        1) DO NOT drink the water it's not what it seems .
+        2) If they hear your voice , they'll know you don't belong.
+        3) You can't use the main door to escape , you must use the backdoor which is through the kitchen.
+        4) Don't get caught in the kitchen or they will kill you.''')
+
+    print('''
+            The waiter comes to drop off your food .''')
+
+    b= int(input('''
+    What do you do?
+    1)Take a sip of water to calm yourself down.
+    2) Smile and pretend to eat .
+    3) Eat whatever he give you .
+    4) Ask why he doesn't have a face .
+    You choice :-'''))
+    m=1
+    while (m):
+        if b==1:
+            print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                                It was not water...it was concentrated sulphuric acid . ''')
+            break
+        elif b==3:
+            print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                                    The food was poisoned , you vomit blood out . ''')
+            break
+        elif b==4:
+            print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+            The waiter knows you don't belong and smashes you head in the plate , and chops your  head off. ''')
+            break
+        else:
+            print ('''                  Good Job <3 (2/5 stages completed)(*^▽^*)                     ''')
+        m-=1
+        
+            
+
+        
+
+        print('''Suddenely a girl from the table behind you stands up and walks towards you, you see her eyes are normal , 
+        she sits with you and points towards the kitchen door .''')
+
+        c= int(input('''
+        What do you do?
+        1)Follow her to the kitchen .
+        2) Ask her to wait .
+        3)Ignore her .
+        4)Geture to wait until the waiter is gone .
+        You choice :-'''))
+        l=1
+        while (l):
+            if c==1:
+                print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                                    The waiter sees you, and gauges you eyes with a spoon. ''')
+                break
+            elif c==2:
+                print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                    The waiter hears you,  and smashes you head in the plate , and chops your  head off. ''')
+                break
+            elif c==3:
+                print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                                She left without you , causing you to be stuck here forever  ''')
+                break
+            else:
+                print ('''                    Good Job <3 (3/5 stages completed)(*^▽^*)                     ''')
+            l-=1
+            
+
+            print('''When the waiter is no watching , you and the girl run towards the kitchen. The waiter notices something suspicious .
+            He is approaching the kitchen.''')
+
+            print('''When the waiter is no watching , you and the girl run towards the kitchen. In the kitchen 
+            you see head shoved into meat grinders. The waiter notices something suspicious . He is approaching the kitchen.''')
+
+            d= int(input('''
+            What do you do?
+            1) Hide behind the fridge and be quiet.
+            2) Run out of the kitchen and pretend nothing happened
+            3) Stay there and pretend you got lost 
+            4) pick up knife and get ready to attack.
+            You choice :-'''))
+            k=1
+            while (k):
+                if d==2:
+                    print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                                The waiter sees you exiting , and tears your limbs apart. ''')
+                    break
+                elif d==3:
+                    print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                                The waiter caught you , and pushes you in the meat grinder. ''')
+                    break
+                elif d==4:
+                    print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                            The waiter doesn't die from your stab , but stabs you with same knife multiple times. ''')
+                    break
+                else:
+                    print ('''                  Good Job <3 (4/5 stages completed) (*^▽^*)                    ''')
+                k-=1
+                
+
+
+                print('''
+                You find the key to the backdoor , you and the girl pick the key up and start heading towards the door . But suddenely you notice 
+                the waiter who didn't leave the kitchen yet. he starts rushing towards you and the girl.''')
+
+                e= int(input('''
+                What do you do?
+                1) Try to hide and dodge the waiter.
+                2) Run out of the kitchen and in the diner .
+                3) Run towards the door . 
+                4) Scream to attract the attention of the diners. 
+                You choice :-'''))
+                j=1
+                while (j):
+                    if e==1:
+                        print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                                The waiter drags you out of your hiding spot and burns your face by blowtorching it. ''')
+                        break
+                    elif e==2:
+                        print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                                The other people sitting in the diner rush towards you and squeeze your eyes out. ''')
+                        break
+                    elif e==4:
+                        print ('''
+                                                You died !!!╯°□°）╯︵ ┻━┻
+                                        The people in the diner rush in the kitchen to eat you alive. ''')
+                        break
+                    else:
+                            print ('''         Good Job <3 (5/5 stages completed) (*^▽^*)                    
+                                        You escaped the diner !!!!!! You're safe .....for now.  ο(=•ω＜=)ρ⌒☆''')
+                            j-=1
+                    break
+                    
+
+                
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
